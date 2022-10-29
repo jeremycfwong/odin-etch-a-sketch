f83c60ba48975d088f54514f789bc5342182ab44
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2022-10-29 11:58:25 +0800

    Add dynamically scaling drawing box
    as title

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-var element = document.getElementById("container")
 
-for (i = 0; i < 16; i++){
-    console.log(i)
-    var row = document.createElement("div");
-    row.setAttribute('class', 'row')
-    element.append(row)
-    for (j = 0; j < 16; j++){
-        var sqr = document.createElement("div");
-        sqr.setAttribute('class','square')
-        row.appendChild(sqr);
+
+function promptInput(){
+    var val = Number(window.prompt("How large do you want the grid to be? (Max is 100x100)", "20"))
+    document.getElementById("container").innerHTML= '';
+    createSquare(val)
+}
+
+function createSquare(val = 16){
+    var element = document.getElementById("container")
+    var size = Math.round(960 / val *10)/10
+
+
+    for (i = 0; i < val; i++){
+        console.log(i)
+        var row = document.createElement("div");
+        row.setAttribute('class', 'row')
+        element.append(row)
+        
+        for (j = 0; j < val; j++){
+            var sqr = document.createElement("div");
+            sqr.setAttribute('class','square');
+            sqr.style.width = size + 'px'
+            sqr.style.height = (size - 2)  + 'px'
+            row.appendChild(sqr);
+        }
     }
 }
+
+createSquare()
